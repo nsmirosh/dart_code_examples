30d6b5d66072ae160b0bfa18c9460c5a6ef9f244
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2020-08-10 15:08:41 +0300

    More changes

--- future_showcase.dart
+++ future_showcase.dart
@@ -1,7 +1,7 @@
 void main() {
   print('print statement before invoking the future');
 
-  Future.delayed(Duration(seconds: 5), () => print('balls'));
+  Future.delayed(Duration(seconds: 5), () => print('Future completed!'));
 
   print('print statement after invoking the future');
 }
